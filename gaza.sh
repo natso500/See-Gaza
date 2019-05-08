5f754d11f432fec5007d5ab3b0c0be9016b68ba3
natso500 <49106380+natso500@users.noreply.github.com>
2019-05-08 23:12:47 +0300

    Add files via upload

---
+++ gaza.sh
@@ -0,0 +1,20 @@
+####sajed###
+clear
+figlet See Gaza
+echo "cereat py DEKESTER"
+echo "Email : natsonatso50@gmail.com"
+echo "follow me instagram : @s.1.aj"
+echo ""
+echo  "-1 info iP ==>>"
+echo "-2 Exit ==>>"
+echo -e $green 
+read -p "Enter namber : " sajed
+if [ $sajed = 1 ];then
+read -p "Enter ip : " ip
+curl http://ip-api.com
+elif [ $sajed = 2 ];then
+exit
+fi 
+
+
+
---
+++ install.sh
@@ -0,0 +1,3 @@
+##bin/bash
+apt-get install curl 
+apt-get install curl 
