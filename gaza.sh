f4c7f3c4c25004447ed103aa2c9ef5d24eb67057
natso500 <49106380+natso500@users.noreply.github.com>
2020-10-07 06:24:42 +0300

    Update gaza.sh

--- gaza.sh
+++ gaza.sh
@@ -1,18 +1,17 @@
-####Mr.Natso50
+####Mr.Natso50####
 clear
 figlet See Gaza
 echo "cereat py DEKESTER"
 echo "Email : natsonatso50@gmail.com"
-echo "follow me instagram : @s.1.aj"
 echo ""
 echo  "-1 info iP ==>>"
 echo "-2 Exit ==>>"
 echo -e $green 
-read -p "Enter namber : " sajed
-if [ $sajed = 1 ];then
+read -p "Enter namber : " moon
+if [ $moon = 1 ];then
 read -p "Enter ip : " ip
 curl http://ip-api.com
-elif [ $sajed = 2 ];then
+elif [ $moon = 2 ];then
 exit
 fi 
 
